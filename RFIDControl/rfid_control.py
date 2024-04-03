902dd023c24d5fd0d15003e4d01ebdb1939de0ce
Adrian Llanes <ados.adrian@gmail.com>
2024-04-03 15:46:22 -0300

    mejorado rfid_control para manejar las lecturas. mejorado router para threading, guardado y devolucion de phys solo entre primer y segunda leida de cada tarjeta nueva

--- RFIDControl/rfid_control.py
+++ RFIDControl/rfid_control.py
@@ -1,13 +1,9 @@
-import threading
-from evdev import InputDevice, categorize, ecodes, list_devices
+from evdev import InputDevice, list_devices, ecodes, categorize
 from pydantic import BaseModel
 
-# Store the card data and associated phys_port
-class DatosLectorUsado(BaseModel):
-    rfid: int
-    phys: str
-
-card_data:DatosLectorUsado = None
+class CardInfo(BaseModel):
+    card_number: int
+    phys_port: str
 
 def get_keyboards() -> list[InputDevice]:
     devices = [InputDevice(path) for path in list_devices()]
@@ -16,7 +12,6 @@ def get_keyboards() -> list[InputDevice]:
         if device.name.find('RFID') >= 0:
             if device.name.find('Keyboard') >= 0:
                 rfid_keyboards.append(device)
-    
     print(f'Se detectaron {len(rfid_keyboards)} lectores RFID')
     return rfid_keyboards
 
@@ -25,42 +20,11 @@ def get_keyboard_ports() -> list[str]:
     puertos = [device.phys for device in devices]
     return puertos
 
-def get_saved_phys_port(card_number: int) -> str:
-    global card_data
-    if card_data is None:
-        return None
-    if card_data.rfid != card_number:
-        return None
-    
-    phys_port = card_data.phys
-    card_data = None
-    return phys_port
-
-def capture_rfid_data(reader_path: str):
-    reader_device = InputDevice(reader_path)
-    print(f"Capturing RFID card data from reader: {reader_device.phys}")
-    
+def read_card(device: InputDevice) -> str:
     card_number = ""
-    while True:
-        for event in reader_device.read_loop():
-            if event.type == ecodes.EV_KEY and event.value == 1:
-                if event.code == ecodes.KEY_ENTER:
-                    if card_number:
-                        save_card_data(card_number, reader_device.phys)
-                        card_number = ""
-                else:
-                    card_number += categorize(event).keycode[len("KEY_"):]
-
-def save_card_data(card_number, phys_port):
-    global card_data
-    card_data = DatosLectorUsado(
-        rfid = card_number,
-        phys = phys_port
-    )
-    # print(f'Tarjeta leida {card_data.rfid} desde puerto: {card_data.phys} guardada')
-
-def start_rfid_capture():
-    for keyboard in get_keyboards():
-        threading.Thread(
-            target=capture_rfid_data, args=(keyboard.path,)
-        ).start()
+    for event in device.read_loop():
+        if event.type == ecodes.EV_KEY and event.value == 1:
+            if event.code == ecodes.KEY_ENTER:
+                return card_number.lstrip("0")
+            else:
+                card_number += categorize(event).keycode[len("KEY_"):]
--- RFIDControl/rfid_router.py
+++ RFIDControl/rfid_router.py
@@ -1,17 +1,40 @@
-from fastapi import APIRouter, HTTPException
-from .rfid_control import get_keyboard_ports, get_saved_phys_port
+from fastapi import APIRouter, Depends
+from .rfid_control import get_keyboards, get_keyboard_ports, read_card, CardInfo
+import threading
+import requests
 
 router = APIRouter()
 
-@router.get("/keyboard_ports")
-def keyboard_ports():
-    ports = get_keyboard_ports()
-    return {"keyboard_ports": ports}
+saved_card = None
+card_read_once = False
 
-@router.post("/get_phys_port")
-def phys_port(card_number: int):
-    phys_port = get_saved_phys_port(card_number)
-    if phys_port:
-        return {"phys_port": phys_port}
-    else:
-        raise HTTPException(status_code=404, detail="Card data not found")
+@router.get("/keyboard_ports")
+def get_ports():
+    return get_keyboard_ports()
+
+@router.get("/get_phys_port")
+def get_phys_port(card_number: int):
+    global saved_card, card_read_once
+    if saved_card and saved_card.card_number == card_number and not card_read_once:
+        card_read_once = True
+        return saved_card.phys_port
+    return None
+
+def watch_keyboard(device):
+    global saved_card, card_read_once
+    while True:
+        card_number = read_card(device)
+        if not saved_card or saved_card.card_number != int(card_number):
+            saved_card = CardInfo(card_number=int(card_number), phys_port=device.phys)
+            card_read_once = False
+        else:
+            saved_card.phys_port = device.phys
+            print(f'posteando a frontend: {saved_card}')
+            # requests.post("http://localhost:3000/inform-card-read", json=saved_card.dict())
+
+def start_keyboard_threads():
+    keyboards = get_keyboards()
+    for keyboard in keyboards:
+        thread = threading.Thread(target=watch_keyboard, args=(keyboard,))
+        thread.daemon = True
+        thread.start()
