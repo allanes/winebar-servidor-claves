43788196a933cf135ff59238ccd59535b61634db
Adrian Llanes <ados.adrian@gmail.com>
2024-04-06 13:07:41 -0300

    agregado pusher de ServerSentEvent para lecturas consecutivas de tarjeta

--- RFIDControl/rfid_router.py
+++ RFIDControl/rfid_router.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import StreamingResponse
 from .rfid_control import get_keyboards, get_keyboard_ports, read_card, CardInfo
 import threading
-import requests
+import asyncio
 
 router = APIRouter()
 
@@ -20,6 +21,24 @@ def get_phys_port(card_number: int):
         return saved_card.phys_port
     return None
 
+async def event_stream(request):
+    while True:
+        if hasattr(request.app.state, 'card_data'):
+            yield f"data: {request.app.state.card_data.json()}\n\n"
+            delattr(request.app.state, 'card_data')
+        await asyncio.sleep(1)
+
+@router.get("/card-read-stream")
+async def card_read_stream(request: Request):
+    return StreamingResponse(event_stream(request), media_type="text/event-stream")
+
+@router.post("/clear-card-data")
+async def clear_card_data():
+    global saved_card, card_read_once
+    saved_card = None
+    card_read_once = False
+    return {"message": "Card data cleared"}
+
 def watch_keyboard(device):
     global saved_card, card_read_once
     while True:
@@ -29,8 +48,12 @@ def watch_keyboard(device):
             card_read_once = False
         else:
             saved_card.phys_port = device.phys
-            print(f'posteando a frontend: {saved_card}')
-            # requests.post("http://localhost:3000/inform-card-read", json=saved_card.dict())
+            asyncio.run(send_sse_event(saved_card))
+
+async def send_sse_event(card_data: CardInfo):
+    from main import app
+    app.state.card_data = card_data
+    print(f'estado de app cambiado a: {card_data}')
 
 def start_keyboard_threads():
     keyboards = get_keyboards()
